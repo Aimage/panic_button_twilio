59c004cb96a55252c0fb0f6d968aa5359373917c
AinaJ <rainanirina@gmail.com>
2023-10-03 21:28:23 +0200

    quit on system with enter + gpiocleanup

--- push_button_call.py
+++ push_button_call.py
@@ -10,8 +10,8 @@ HELP_TEXT = "Please help!! I have an emergency problem!"
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
-AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', "")
+AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', "")
 
 
 def call_relative(client, relaive_number):
@@ -35,3 +35,10 @@ GPIO.setup(
     10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN
 )  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(10, GPIO.RISING, callback=call_for_help)
+
+
+print("System is running, press the push button to call your relatives or")
+
+message = input("Press enter to quit\n\n")
+
+GPIO.cleanup()
